254c82a2d527645765006e90df9f0aefb4c952f6
skravchuk <ext-serhii.krachuk@here.com>
2024-08-18 14:23:27 +0300

    project setup

---
+++ src/main/java/org/example/kt_tasks/KtTasksApplication.java
@@ -0,0 +1,13 @@
+package org.example.kt_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtTasksApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KtTasksApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/kt_tasks/KtTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kt_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtTasksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

590a66a54af8a8ca9ecf761df6c2ccccba932bc8
skravchuk <ext-serhii.krachuk@here.com>
2024-08-29 09:33:53 +0300

    stream to browser: alternative, more robust SpringApplication setup

--- src/main/java/org/example/kt_tasks/KtTasksApplication.java
+++ src/main/java/org/example/kt_tasks/KtTasksApplication.java
@@ -1,6 +1,7 @@
 package org.example.kt_tasks;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +9,13 @@ public class KtTasksApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(KtTasksApplication.class, args);
+
+    //Alternative SpringApplication creation to explicitly set WebApplicationType in order to avoid internal errors
+//    SpringApplication springApplication = new SpringApplication(KtTasksApplication.class);
+//    springApplication.setWebApplicationType(WebApplicationType.SERVLET);
+//    //OR
+//    springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+//    springApplication.run(args);
   }
 
 }
